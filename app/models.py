0d168012ebd1cdb14fc9b032c5b6f745c4e98a68
Matthieu BERTRAND <xplode-ccm@hotmail.fr>
2018-11-30 09:01:10 +0100

    Add relationship in models

--- app/models.py
+++ app/models.py
@@ -6,10 +6,12 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(280))
+    email = db.Column(db.String(200))
     api_token = db.Column(db.String(280))
+    tweets = db.relationship('Tweet', back_populates="user")
 
     def __repr__(self):
-        return f"<User #{self.id}>"
+        return f"<User #{self.username}>"
 
 class Tweet(db.Model):
     __tablename__ = "tweets"
@@ -17,6 +19,7 @@ class Tweet(db.Model):
     text = db.Column(db.String(280))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="tweets")
 
     def __repr__(self):
         return f"<Tweet #{self.id}>"
